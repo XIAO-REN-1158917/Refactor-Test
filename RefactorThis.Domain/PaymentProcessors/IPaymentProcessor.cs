c53f287e03bef2015137dcaae38f6dbb11042a9f
Gary Ren <espresso1014@gmail.com>
2025-05-06 22:09:54 +1200

    Create payment processor

---
+++ RefactorThis.Domain/PaymentProcessors/IPaymentProcessor.cs
@@ -0,0 +1,9 @@
+﻿using RefactorThis.Persistence;
+
+namespace RefactorThis.Domain.PaymentProcessors
+{
+    public interface IPaymentProcessor
+    {
+        void ConfirmPayment(Invoice invoice, Payment payment);
+    }
+}
---
+++ RefactorThis.Domain/PaymentProcessors/StandardInvoicePaymentProcessor.cs
@@ -0,0 +1,17 @@
+﻿using RefactorThis.Persistence;
+using System.Collections.Generic;
+
+namespace RefactorThis.Domain.PaymentProcessors
+{
+    /// <summary>
+    /// Process standard invoice payment.
+    /// </summary>
+    public class StandardInvoicePaymentProcessor : IPaymentProcessor
+    {
+        public void ConfirmPayment(Invoice invoice, Payment payment)
+        {
+            invoice.AmountPaid += payment.Amount;
+            invoice.Payments.Add(payment);
+        }
+    }
+}
